73c9721d5afffcd24b58addc99de5feb49907adb
mouse_484 <escmouse484@gmail.com>
2019-09-17 03:37:19 +0000

    ✨ : Add code bot Activity

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
     console.log(`Ready, ${client.user.tag}`);
+    client.user.setActivity("質問、意見箱です気軽にDMにメッセージを送信してください");
 });
 
 client.login(process.env.TOKEN);
